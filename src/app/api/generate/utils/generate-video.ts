a0e4a9f0dfca581acf5fe1703caff24823542dda
Yiork <yiorkdev@gmail.com>
2025-07-07 22:31:35 -0400

    improve-architecture

--- src/app/api/generate/utils/generate-video.ts
+++ src/app/api/generate/utils/generate-video.ts
@@ -1,42 +1,42 @@
-import { mock } from "@/app/api/generate/utils/mock";
-
-type GenerateVideo = {
-  prompt: string;
-  voiceId: string;
-  aspectRatio: string;
-};
-
-export async function generateVideo({
-  prompt,
-  voiceId,
-  aspectRatio,
-}: GenerateVideo): Promise<Omit<Video, "id">> {
-  // console.log("input", prompt, voiceId, aspectRatio);
-  //
-  // const response = await fetch(
-  //   `${process.env.MASTRA_URL}/api/workflows/remotionWorkflow/start-async`,
-  //   {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/json" },
-  //     body: JSON.stringify({
-  //       inputData: {
-  //         userRequest: {
-  //           prompt,
-  //           voiceId,
-  //           aspectRatio,
-  //         },
-  //       },
-  //       runtimeContext: {},
-  //     }),
-  //   },
-  // );
-  //
-  // const data = await response.json();
-  //
-  // if (data.status !== "success") {
-  //   console.log("API Error:", data);
-  //   throw new Error(`Video generation failed with status: ${response.status}`);
-  // }
-
-  return mock;
-}
+// import { mock } from "@/app/api/generate/utils/mock";
+//
+// type GenerateVideo = {
+//   prompt: string;
+//   voiceId: string;
+//   aspectRatio: string;
+// };
+//
+// export async function generateVideo({
+//   prompt,
+//   voiceId,
+//   aspectRatio,
+// }: GenerateVideo): Promise<Omit<Video, "id">> {
+//   // console.log("input", prompt, voiceId, aspectRatio);
+//   //
+//   // const response = await fetch(
+//   //   `${process.env.MASTRA_URL}/api/workflows/remotionWorkflow/start-async`,
+//   //   {
+//   //     method: "POST",
+//   //     headers: { "Content-Type": "application/json" },
+//   //     body: JSON.stringify({
+//   //       inputData: {
+//   //         userRequest: {
+//   //           prompt,
+//   //           voiceId,
+//   //           aspectRatio,
+//   //         },
+//   //       },
+//   //       runtimeContext: {},
+//   //     }),
+//   //   },
+//   // );
+//   //
+//   // const data = await response.json();
+//   //
+//   // if (data.status !== "success") {
+//   //   console.log("API Error:", data);
+//   //   throw new Error(`Video generation failed with status: ${response.status}`);
+//   // }
+//
+//   return mock;
+// }
