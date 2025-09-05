8282a73bf8b9e0e687f3011e920d8b4a5eec5291
Yiork <yiorkdev@gmail.com>
2025-09-05 08:25:59 -0400

    update layout

--- src/components/home-page/index.tsx
+++ src/components/home-page/index.tsx
@@ -11,7 +11,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
 import { gridBase, Tile } from "../bento";
-import { VideoDialog } from "../video-dialog";
 
 export const HomePage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -88,18 +87,17 @@ export const HomePage = () => {
         </div>
       </section>
 
-      {/* bento */}
       <section className="reveal">
-        <div className="flex flex-col gap-5 pt-24">
-          <div className="flex items-center">
+        <div className="flex flex-col xl:items-start items-center gap-5 pt-24">
+          <div className="flex justify-center xl:justify-start items-center">
             <div className="text-fuchsia-900 bg-fuchsia-100 px-5 h-10 rounded-full flex items-center justify-center">
               Transform Words Into Motion
             </div>
           </div>
-          <h2 className="text-5xl font-neue-montreal font-bold max-w-xl leading-[3.5rem]">
+          <h2 className="text-5xl font-neue-montreal font-bold max-w-xl leading-[3.5rem] text-center xl:text-left">
             Animate text from a simple prompt.
           </h2>
-          <h3 className="text-xl leading-relaxed font-medium max-w-xl text-muted-foreground">
+          <h3 className="text-xl leading-relaxed font-medium max-w-xl text-muted-foreground text-center xl:text-left">
             Skip the keyframes—just describe how you want your text to move.
             GoMotion instantly animates titles, captions, and logos with
             cinematic flair. Perfect for product demo and creators who want
@@ -132,10 +130,15 @@ export const HomePage = () => {
             title="Symphony of Motion"
             description="Create a colorful kinetic typography animation with a text 'Symphony of Motion'"
           />
+          <Tile
+            src="https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/nike-short.mp4"
+            title="Nike Short"
+            description="Create a nike short animation with a text 'Nike Short'"
+          />
         </div>
       </section>
 
-      <section className="reveal justify-center bg-neutral-100 py-20 flex flex-col gap-10 md:gap-16 px-5 md:px-14 rounded-3xl">
+      <section className="reveal justify-center bg-neutral-100 py-20 flex flex-col gap-10 md:gap-16 px-5 md:px-14 rounded-3xl mb-24">
         <h2 className="text-6xl text-center font-neue-montreal font-bold leading-[3.5rem]">
           Discover the Narrative Mode
         </h2>
@@ -147,29 +150,22 @@ export const HomePage = () => {
             autoPlay
             muted
             loop
-            className="w-full h-full object-cove"
+            className="w-full h-full"
           />
         </div>
 
         <div className="flex flex-col-reverse xl:flex-row gap-10 md:gap-16">
           <div className="w-full xl:w-2/3 flex items-center justify-center">
             <div className="flex flex-col items-center justify-center">
-              <VideoDialog
+              <video
                 src="https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/jdi.mp4"
-                title="JDI"
-                description="Create a complete nike ad with a stunning sports style image"
-                className="sm:h-[24rem]"
-              >
-                <video
-                  src="https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/jdi.mp4"
-                  preload="metadata"
-                  playsInline
-                  autoPlay
-                  muted
-                  loop
-                  className="w-full h-full object-cover"
-                />
-              </VideoDialog>
+                preload="metadata"
+                playsInline
+                autoPlay
+                muted
+                loop
+                className="w-full h-full object-cover"
+              />
             </div>
           </div>
 
@@ -179,7 +175,7 @@ export const HomePage = () => {
             </h2>
 
             <div className="flex items-center">
-              <div className="text-emerald-900 bg-emerald-100  px-5 h-10 rounded-full flex items-center justify-center">
+              <div className="text-emerald-900 bg-emerald-100 px-5 h-10 rounded-full flex items-center justify-center">
                 Master Shape Dynamics
               </div>
             </div>
@@ -193,35 +189,91 @@ export const HomePage = () => {
         </div>
       </section>
 
-      {/* list */}
-      <section className="reveal mb-20">
+      {/* Design for any screen  */}
+      <section className="reveal flex flex-col xl:flex-row items-center justify-between mb-20">
         <div className="flex flex-col gap-5 pt-24 mb-20">
-          <div className="flex items-center">
+          <div className="flex items-center justify-center xl:justify-start">
             <div className="text-amber-900 bg-amber-100  px-5 h-10 rounded-full flex items-center justify-center">
-              Master Shape Dynamics
+              Design for any screen
             </div>
           </div>
 
-          <h2 className="text-5xl font-neue-montreal font-bold max-w-xl leading-[3.5rem]">
-            Move shapes in complex animations
+          <h2 className="text-5xl text-center xl:text-left font-neue-montreal font-bold max-w-xl leading-[3.5rem]">
+            Designed for every screen from mobile to desktop.
           </h2>
-          <h3 className="text-xl leading-relaxed font-medium max-w-xl text-muted-foreground">
-            Create advanced shape animations in seconds. From geometric morphs
-            to fluid motion, GoMotion handles the complexity so you can focus on
-            creativity. Great for explainers, data viz, and artistic visuals
+          <h3 className="text-xl text-center xl:text-left leading-relaxed font-medium max-w-xl text-muted-foreground">
+            Your audience is everywhere and your content should be too. Create
+            animations for all screen sizes, from smartphones, tablets to
+            desktop displays. Whether designing a product teaser for Instagram
+            or TikTok your video will look perfectly aligned.
           </h3>
         </div>
 
-        <div
-          className={cn("grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-5")}
-        >
+        <video
+          src="https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/phone-reveal.mp4"
+          preload="metadata"
+          playsInline
+          autoPlay
+          muted
+          loop
+          className="w-auto h-[35rem] border-4 border-amber-500 rounded-3xl"
+        />
+      </section>
+
+      {/* Import your brand*/}
+      <section className="reveal mb-20 flex flex-col items-center">
+        <div className="flex flex-col items-center gap-5 pt-24 mb-20">
+          <div className="flex items-center">
+            <div className="text-indigo-900 bg-indigo-100 text-center px-5 h-10 rounded-full flex items-center justify-center">
+              Animation for your brand
+            </div>
+          </div>
+
+          <h2 className="text-5xl text-center font-neue-montreal font-bold max-w-xl leading-[3.5rem]">
+            Import your brand in one click
+          </h2>
+          <h3 className="text-xl text-center leading-relaxed font-medium max-w-xl text-muted-foreground">
+            Easily import your brand to create videos from a simple image.
+            Perfect for social media, product launches, ads, and more.
+          </h3>
+        </div>
+
+        <video
+          src="https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/brand-import.mp4"
+          preload="metadata"
+          playsInline
+          autoPlay
+          muted
+          loop
+          className="w-auto h-[35rem] border-4 border-indigo-500 rounded-3xl"
+        />
+      </section>
+
+      {/*  Chart and complex animations */}
+      <section className="reveal mb-20">
+        <div className="flex flex-col xl:items-start items-center gap-5 pt-24 mb-20">
+          <div className="flex justify-center xl:justify-start items-center">
+            <div className="text-fuchsia-900 bg-fuchsia-100 px-5 h-10 rounded-full flex items-center justify-center">
+              Complex animations
+            </div>
+          </div>
+
+          <h2 className="text-5xl font-neue-montreal font-bold max-w-xl leading-[3.5rem] text-center xl:text-left">
+            Create complex animations likes chart and simulation
+          </h2>
+          <h3 className="text-xl leading-relaxed font-medium max-w-xl text-muted-foreground text-center xl:text-left">
+            Create animated charts from any data, ideal for explainers,
+            simulations, and impactful presentations.
+          </h3>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 xl:grid-cols-3">
           {shapesVideo.map((video, idx) => (
             <Tile
               key={video.title + idx}
               src={video.src}
               title={video.title}
               description={video.description}
-              className="sm:h-[18rem]"
             />
           ))}
         </div>
@@ -229,7 +281,7 @@ export const HomePage = () => {
 
       <section className="reveal flex flex-col md:flex-row items-center gap-6 md:gap-10 text-center md:text-left pt-10 pb-32">
         <div className="flex flex-col gap-10">
-          <div className="text-2xl flex flex-col gap-3">
+          <div className="text-2xl flex flex-col gap-3 font-semibold">
             <p>We are young</p>
             <p>We are passionates</p>
             <p>We are storytellers</p>
@@ -238,8 +290,8 @@ export const HomePage = () => {
           </div>
 
           <Link href="/sign-in">
-            <button className="text-emerald-900 hover:bg-emerald-200 transition-colors px-10 duration-300 bg-emerald-100 font-medium text-lg w-full sm:w-60 lg:w-80 h-14 sm:h-16 rounded-full">
-              Get started
+            <button className="text-indigo-900 cursor-pointer flex items-center justify-center gap-2 font-semibold border-2 border-indigo-500 hover:bg-indigo-200 transition-colors px-10 duration-300 bg-indigo-100 text-lg w-full sm:w-60 lg:w-80 h-14 sm:h-16 rounded-xl">
+              Try Gomotion for free <span className="text-2xl"> 🤩</span>
             </button>
           </Link>
         </div>
@@ -250,33 +302,29 @@ export const HomePage = () => {
 
 const shapesVideo = [
   {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/countdown.mov",
-    title: "Countdown",
-    description: "Create a countdown animation with a text 'Countdown'",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/nike-short.mp4",
-    title: "Nike Short",
-    description: "Create a nike short animation with a text 'Nike Short'",
-  },
-  {
     src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/bar-chart-ai-models.mp4",
     title: "Best AI Models",
     description:
       "Create a bar chart representing the best ai models in a white background",
+    className: "col-span-12 md:col-span-4 row-span-1",
+  },
+  {
+    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/chart.mp4",
+    title: "Chart",
+    description: "Create a dynamic chart inspired from this image",
+  },
+  {
+    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/donut.mp4",
+    title: "Donut",
+    description: "Create a donut chart animation fromt this image",
+  },
+  {
+    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/countdown.mov",
+    title: "Countdown",
+    description: "Create a countdown animation with a text 'Countdown'",
+    className: "col-span-12 md:col-span-4 row-span-2",
   },
 
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/cta-button.mov",
-    title: "CTA Button",
-    description:
-      "Create a fancy cta button animation with a cursor clicking on it",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/data-science-evolution.mp4",
-    title: "Data Science Evolution",
-    description: "Create chart showing the evolution of data science",
-  },
   {
     src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/iphone-message.mp4",
     title: "Iphone Message",
@@ -291,33 +339,5 @@ const shapesVideo = [
     src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/cryptic.mp4",
     title: "Cryptic",
     description: "Create futuristic animation with a text 'Cryptic'",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/hydrate.mp4",
-    title: "Hydrate Sachets",
-    description:
-      "Create a clean animation for our brand Active8, which produces hydro sachets, Ensure the brand colors are respected",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/chart.mp4",
-    title: "Chart",
-    description: "Create a dynamic chart inspired from this image",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/donut.mp4",
-    title: "Donut",
-    description: "Create a donut chart animation fromt this image",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/phone-reveal.mp4",
-    title: "IPhone Calendar Reveal",
-    description:
-      "Create an iphone reveal animation, with a calendar set on Thursday 15th displaying the text 'Book grow your mind. Read to lead', include a growing counter from 0 to 49",
-  },
-  {
-    src: "https://uftbovflyenqlyxvpybv.supabase.co/storage/v1/object/public/website-assets/bar-color.mp4",
-    title: "Bar Color",
-    description:
-      "Create a bar chart animation  showing the distribution of images, video and dynamic content, with value 5, 4, 2 respectively for April",
   },
 ];
